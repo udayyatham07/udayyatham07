68f3cad497fd180cffb8be48ce4fe4d7b39e3cb5
pbprasad2329 <pbprasad2329@gmail.com>
2023-11-06 12:22:44 +0530

    terrafrom

---
+++ insance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  
+
+  tags = {
+    Name        = "MyEC2Instance"
+    Environment = "Production"
+    Owner       = "YourName"
+  }
+}
